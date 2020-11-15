5e50cec22073a123a7173e0d90f6af154503e20a
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-11-15 21:42:23 +0530

    splash

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -6,24 +6,57 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
+  final Shader linearGradient = LinearGradient(
+    colors: <Color>[
+      Color(0xffDA44bb),
+      Color(0xffF58529),
+      Color(0xffDD2A7B),
+      Color(0xff8134AF)
+    ],
+  ).createShader(Rect.fromLTWH(100.0, 20.0, 200.0, 70.0));
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Expanded(
+      body: Container(
+        color: Colors.black.withOpacity(0.94),
+        child: Column(
+          children: [
+            Expanded(
               child: Container(
-            color: Colors.black.withOpacity(0.94),
-            child: Center(
-              child: Image.asset(
-                "images/instaLogo.png",
-                height: 80,
-                width: 80,
+                child: Center(
+                  child: Image.asset(
+                    "images/instaLogo.png",
+                    height: 80,
+                    width: 80,
+                  ),
+                ),
               ),
             ),
-          ))
-        ],
+            Container(
+                height: MediaQuery.of(context).size.height * 0.12,
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    Text(
+                      "from",
+                      style: TextStyle(color: Colors.white54, fontSize: 15),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "FACEBOOK",
+                      style: TextStyle(
+                          letterSpacing: 2,
+                          foreground: Paint()..shader = linearGradient,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )),
+          ],
+        ),
       ),
     );
   }
